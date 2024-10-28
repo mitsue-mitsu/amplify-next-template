c9186d156b60b6e7edcea0eca6a48ade2da31504
mitsue <mitsueyuto0806y3@gmail.com>
2024-10-28 23:15:44 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { createMap } from 'maplibre-gl-js-amplify';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import { createMap } from "maplibre-gl-js-amplify";
+import "maplibre-gl/dist/maplibre-gl.css";
 import "maplibre-gl-js-amplify/dist/public/amplify-map.css";
+import GoogleMap from "@/component/GoogleMap";
 
 Amplify.configure(outputs);
 
@@ -22,12 +23,12 @@ export default function App() {
 
   async function initializeMap() {
     const map = await createMap({
-      container: 'map', // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/
+      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/
       center: [-123.1187, 49.2819], // [Longitude, Latitude]
-      zoom: 11
+      zoom: 11,
     });
   }
-  
+
   initializeMap();
 
   function listTodos() {
@@ -35,9 +36,9 @@ export default function App() {
       next: (data) => setTodos([...data.items]),
     });
   }
-    
+
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+    client.models.Todo.delete({ id });
   }
 
   useEffect(() => {
@@ -56,9 +57,9 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li 
-            onClick={() => deleteTodo(todo.id)}
-            key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -70,6 +71,7 @@ export default function App() {
       </div>
       <button onClick={signOut}>Sign out</button>
       <div id="map" className="h-1/2"></div>
+      <GoogleMap />
     </main>
   );
 }
